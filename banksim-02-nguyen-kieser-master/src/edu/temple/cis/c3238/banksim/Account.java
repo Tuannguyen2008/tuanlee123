f384b559a81fc33b5765e9c8224e8ef6921dbb29
Tuan Nguyen <tug01026@temple.edu>
2019-10-09 10:16:29 -0400

    Add note

---
+++ banksim-02-nguyen-kieser-master/src/edu/temple/cis/c3238/banksim/Account.java
@@ -0,0 +1,60 @@
+package edu.temple.cis.c3238.banksim;
+
+/**
+ * @author Cay Horstmann
+ * @author Modified by Paul Wolfgang
+ * @author Modified by Charles Wang
+ */
+public class Account {
+
+    private volatile int balance;
+    private final int id;
+    private final Bank myBank;
+
+    public Account(Bank myBank, int id, int initialBalance) {
+        this.myBank = myBank;
+        this.id = id;
+        balance = initialBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public synchronized void WaitForSufficientFunds(int amount){
+        while(amount >= balance){
+            try{
+                wait();
+            }
+            catch(InterruptedException e){
+                System.out.printf("Cannot transfer fund");
+            }
+            
+        }
+    }
+
+    public synchronized boolean withdraw(int amount) {
+        if (amount <= balance) {
+            int currentBalance = balance;
+//            Thread.yield(); // Try to force collision
+            int newBalance = currentBalance - amount;
+            balance = newBalance;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public synchronized void deposit(int amount) {
+        int currentBalance = balance;
+//        Thread.yield();   // Try to force collision
+        int newBalance = currentBalance + amount;
+        balance = newBalance;
+        notifyAll();
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Account[%d] balance %d", id, balance);
+    }
+    
+}
